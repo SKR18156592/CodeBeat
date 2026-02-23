58beb51a73f53c6eeb2b712567433e2b4fd8b2bf
Suman Raj <sumanraj112002@gmail.com>
2026-02-23 14:35:40 +0530

    add modify_function

--- codebeat/code_tracker_class.py
+++ codebeat/code_tracker_class.py
@@ -1,4 +1,15 @@
 
-
+import inspect
 class code_tracker:
-    pass
+    def __init__(self,function_object):
+        self.function_object=function_object
+        pass
+    def modify_function(self):
+        pass
+
+
+        
+        
+
+
+    
